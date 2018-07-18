ecfb6f99e39902b93f18bc265983164f2697612b
Hasnen tai <hasnentai@Hasnens-MacBook.local>
2018-07-18 18:32:18 +0530

    bhai sab kiya he merge ho jana

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ElementRef, Renderer2 } from '@angular/core';
 import {
   trigger,
   state,
@@ -6,6 +6,7 @@ import {
   animate,
   transition
 } from '@angular/animations';
+import { HostListener } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -27,30 +28,53 @@ import {
   ]
 })
 export class HeaderComponent {
+
   public anistate = 'inactive';
+  content: any;
+  selectedIndex = 0;
+
+  constructor(public el: ElementRef, public renderer: Renderer2) {}
+
   myitem = [
     {
-      menu : 'Home'
+      menu : 'Home',
+      link : '/home'
     },
     {
-      menu : 'DashBoard'
+      menu : 'DashBoard',
+      link : '/profilepage'
     },
     {
-      menu : 'Courses Taken'
+      menu : 'Courses Taken',
+      link : '/gmail'
     },
     {
-      menu : 'Reports'
+      menu : 'Reports',
+      link : '/google'
     },
     {
-      menu : 'Edit Info'
+      menu : 'Edit Info',
+      link : '/edit'
     },
     {
-      menu : 'Log Out'
+      menu : 'Log Out',
+      link : '/you'
     },
   ];
-  constructor() { }
 
-  selectedIndex = 0;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll($event) {
+    if (window.pageYOffset >= 100) {
+      this.content = this.el.nativeElement.getElementsByTagName('nav')[0];
+      this.renderer.addClass(this.content, 'after-scroll-header');
+      console.log(this.content);
+    }  else {
+      this.renderer.removeClass(this.content, 'after-scroll-header');
+    }
+  }
+
+
   select(index: number) {
       this.selectedIndex = index;
   }
