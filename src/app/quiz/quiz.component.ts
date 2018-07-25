07825344bc31a9cbf71681d877a5f6c39ff7d365
G@G@|\| <gaganb800@gmail.com>
2018-07-25 12:33:01 +0530

    Quiz page ts done

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  Renderer,
+  Renderer2,
+  HostListener
+} from '@angular/core';
 
 @Component({
   selector: 'app-quiz',
@@ -7,9 +14,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  constructor() { }
+  constructor(public el: ElementRef, public renderer: Renderer2) { }
+  imagepresent = true;
+  imagediv: any;
+  optionsdiv: any;
 
   ngOnInit() {
+      this.imagediv = this.el.nativeElement.getElementsByClassName('imagecontainer')[0];
+      this.optionsdiv = this.el.nativeElement.getElementsByClassName('listclass')[0];
+      console.log(this.imagediv);
+    if (!this.imagepresent) {
+      this.renderer.addClass(this.imagediv, 'imagedisplaynone');
+      this.renderer.removeClass(this.optionsdiv, 'listclass');
+      this.renderer.addClass(this.optionsdiv, 'optionsnoimage');
+    }
   }
-
 }
