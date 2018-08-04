b1cbc6b2f7129ac86056e096973e4ef4a003cec1
G@G@|\| <gaganb800@gmail.com>
2018-08-04 19:14:15 +0530

    after long tie merge

--- src/app/serverservice.service.ts
+++ src/app/serverservice.service.ts
@@ -17,7 +17,7 @@ export class ServerserviceService  implements OnInit {
   currentModuleCounter = 0;
   httpOptions;
   userToken;
-  readonly _rootUrl = 'http://13.232.35.15';
+  readonly _rootUrl = 'https://businesstoys.tech';
   constructor(private http: HttpClient ) { }
   getToken(): string {
     return localStorage.getItem('token');
